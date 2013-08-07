64dbad7a5012f7805482e5c7469d259b0f2877de
Dan Collis-Puro <djcp@thoughtbot.com>
2013-08-07 17:44:41 -0400

    inotify ctags updater test.

---
+++ bin/inotify_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+WATCHED_DIR=$HOME/code/work/chillingeffects
+
+inotifywait --exclude=tags --exclude="/tags.*|log/|tmp/|\.git/|coverage/|doc/"\
+  -m -r -e modify -e move -e create -e delete $WATCHED_DIR | while read line
+do
+  ctags -f $WATCHED_DIR/tags -R --exclude='*.js' --langmap='ruby:+.rake.builder.rjs' --languages=-javascript $WATCHED_DIR/
+  DATE=date
+  echo "updated because of $line on $DATE"
+done
