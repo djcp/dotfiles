512b58b82be80e4d369c5cb0ef1f4e826fb96500
Dan Collis-Puro <djcp@thoughtbot.com>
2014-01-27 14:12:17 -0500

    i3wm improvements - colored status bar, sed-based streaming JSON parsing.

--- bin/i3status_custom.sh
+++ bin/i3status_custom.sh
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+SEPARATOR_BLOCK_WIDTH=31
+
+insert_weather_json() {
+  WEATHER_JSON=$(cat ~/.cli-weather-forecast)
+  FIXED_LINE="[{\"full_text\": \"$WEATHER_JSON\"}, $@,"
+}
+
+insert_separator_width() {
+  FIXED_LINE=$(echo $FIXED_LINE | sed "s/{/{\"separator_block_width\": $SEPARATOR_BLOCK_WIDTH, /g")
+}
 
 i3status | while :
 do
-        read line
-        echo "`cat ~/.cli-weather-forecast` | $line" || exit 1
+  read LINE
+
+  if echo "$LINE" | grep -qiE 'name'
+  then
+    FIXED_LINE=$(echo $LINE | sed  '0,/\[/ s/,*\[//')
+    insert_weather_json $FIXED_LINE
+    insert_separator_width $FIXED_LINE
+
+    echo $FIXED_LINE
+  else
+    echo $LINE
+  fi
 done
