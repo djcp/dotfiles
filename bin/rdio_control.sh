a9f623eba23ce83f02bb053211a3d1e6ef0e7ecb
Dan Collis-Puro <djcp@thoughtbot.com>
2014-08-02 12:45:40 -0400

    Better rdio control via xdotool.

---
+++ bin/rdio_control.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+action="$1"
+
+case $action in
+  pause*)
+    key_to_send="space"
+    ;;
+  next*)
+    key_to_send="Right"
+    ;;
+  previous*)
+    key_to_send="Left"
+    ;;
+  *)
+    key_to_send="space"
+    ;;
+esac
+
+current_window=$(xdotool getwindowfocus)
+xdotool search --name Rdio windowactivate --sync -- \
+  key "$key_to_send" \
+  windowactivate "$current_window"
