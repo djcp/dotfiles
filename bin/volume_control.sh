dc564fa09858ef325e7abcd8bc0fa72b52eaa001
Dan Collis-Puro <dan@collispuro.net>
2014-11-07 10:19:12 -0500

    More flexible volume control of pulseaudio sinks

---
+++ bin/volume_control.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+soundbar_sink=$(pacmd list-sinks | grep 'card:' | grep Sound | cut -d: -f 2 | cut -d' ' -f 2)
+action="$1"
+amount="2%"
+
+case $action in
+  up*)
+    direction="+$amount"
+    ;;
+  *)
+    direction="-$amount"
+    ;;
+esac
+
+if [ "$soundbar_sink" = "" ]; then
+  non_soundbar_sink=$(pacmd list-sinks | grep 'index:' | tail -n1 | cut -d: -f 2)
+  sink="$non_soundbar_sink"
+else
+  sink="$soundbar_sink"
+fi
+
+pactl set-sink-volume "$sink" -- "$direction"
