bd59aae3491315e6794dbf5485bf6097e55a3b05
Dan Collis-Puro <dan@collispuro.net>
2015-05-26 14:07:31 -0400

    Post setup improvements, firewall install

--- bin/post-setup-config-tweaks.sh
+++ bin/post-setup-config-tweaks.sh
@@ -6,3 +6,13 @@ sudo dpkg-reconfigure dash
 echo 'Set urxvt as the terminal'
 sudo aptitude install rxvt-unicode-256color
 sudo update-alternatives --config x-terminal-emulator
+
+echo 'open magnet links with qBittorrent'
+xdg-mime default qBittorrent.desktop x-scheme-handler/magnet
+
+echo 'install ntp'
+sudo aptitude install ntp
+
+echo 'install iptables-persistent and relevant rules'
+sudo aptitude install iptables-persistent
+sudo ln -s /home/djcp/code/dotfiles/rules.v4 /etc/iptables/rules.v4
