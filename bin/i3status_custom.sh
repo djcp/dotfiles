b0f060b3cc3812b8c02327e9150f1925576b44ca
Dan Collis-Puro <dan@collispuro.net>
2016-07-14 22:09:18 -0400

    yubikey status addition to i3 status bar

--- bin/i3status_custom.sh
+++ bin/i3status_custom.sh
@@ -2,14 +2,32 @@
 
 SEPARATOR_BLOCK_WIDTH=31
 
+set_yubikey_message(){
+  yubikey_state=`yubitoggle --state`
+  if [ "$?" = '1' ]; then
+    yubikey_output=''
+  else
+    if [ "$yubikey_state" = "0" ]; then
+      yubikey_message="off"
+      yubikey_color="#ff0000"
+    else
+      yubikey_message="on"
+      yubikey_color="#00ff00"
+    fi
+    yubikey_output="🔑 $yubikey_message"
+  fi
+}
+
 insert_json() {
   max_brightness=`cat /sys/class/backlight/intel_backlight/max_brightness`
   current_brightness=`cat /sys/class/backlight/intel_backlight/brightness`
 
+  set_yubikey_message
+
   WEATHER_JSON=$(cat ~/.cli-weather-forecast)
   brightness=$(echo "scale=2;(100 - (($max_brightness - $current_brightness) \
     / $max_brightness) * 100);" | bc);
-  FIXED_LINE="[{\"full_text\": \"$WEATHER_JSON\"}, {\"full_text\": \
+  FIXED_LINE="[{\"full_text\": \"$WEATHER_JSON\"}, {\"full_text\": \"$yubikey_output\", \"color\": \"$yubikey_color\"}, {\"full_text\": \
     \"💡 $brightness\"}, $@,"
 }
 
